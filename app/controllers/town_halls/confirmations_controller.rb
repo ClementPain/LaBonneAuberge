c505770b6a09b130bbd27b5e2611c89020a77d68
clement <clementpain@outlook.com>
2020-09-10 09:45:56 +0200

    failure

--- app/controllers/town_halls/confirmations_controller.rb
+++ app/controllers/town_halls/confirmations_controller.rb
@@ -13,7 +13,15 @@ class TownHalls::ConfirmationsController < Devise::ConfirmationsController
 
   # GET /resource/confirmation?confirmation_token=abcdef
   def show
-    super
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      respond_with_navigational(resource){ redirect_to root_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ redirect_to root_path_for(resource_name, resource) }
+    end
   end
 
   protected
