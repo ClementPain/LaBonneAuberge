a9a953c42ac366a47333c21351d0ceea30d16d52
clement <clementpain@outlook.com>
2020-09-10 23:13:56 +0200

    Villagers Profil

--- app/models/village.rb
+++ app/models/village.rb
@@ -2,6 +2,10 @@ class Village < ApplicationRecord
     has_one :town_hall
     has_many :events
 
+    # lien avec la table villager (profils utilisateurs)
+    has_many :villagers
+    has_many :users, through: :villagers
+
     validates :email, presence: true, uniqueness: true, length: { in: 6..50 }
     validates :name, presence: true, uniqueness: true, length: { in: 2..40 }
     validates :zipcode, presence: true, length: {is: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_144657) do
+ActiveRecord::Schema.define(version: 2020_09_10_204735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,10 +71,6 @@ ActiveRecord::Schema.define(version: 2020_09_10_144657) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_town_halls_on_email", unique: true
@@ -100,6 +96,19 @@ ActiveRecord::Schema.define(version: 2020_09_10_144657) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "villagers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "description"
+    t.datetime "date_of_birth"
+    t.bigint "village_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_villagers_on_user_id"
+    t.index ["village_id"], name: "index_villagers_on_village_id"
+  end
+
   create_table "villages", force: :cascade do |t|
     t.string "name"
     t.string "zipcode"
@@ -110,4 +119,6 @@ ActiveRecord::Schema.define(version: 2020_09_10_144657) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "villagers", "users"
+  add_foreign_key "villagers", "villages"
 end
