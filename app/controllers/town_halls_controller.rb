fb31c962b4473fafc4a470c245b362516000b292
clement <clementpain@outlook.com>
2020-09-11 11:10:45 +0200

    dashboard Events Town Hall

--- app/controllers/town_halls_controller.rb
+++ app/controllers/town_halls_controller.rb
@@ -1,7 +1,7 @@
 class TownHallsController < ApplicationController
   def show
     @village = Village.find_by(email:current_town_hall.email)
-    @events_today = Event.select { |e| e.village === @village && e.start_date = Date.today }
+    @events_today = Event.select { |e| e.village === @village && e.start_date === Date.today }
     @events_to_come = Event.select { |e| e.village === @village && e.start_date > Date.today }
     @events_passed = Event.select { |e| e.village === @village && e.start_date < Date.today }
   end
