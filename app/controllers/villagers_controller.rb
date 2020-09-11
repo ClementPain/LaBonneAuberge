2cb3e2aba31721b72fbc5a3644751fe7d95c327f
Camillia-MEDDAHI <meddah.cam@laposte.net>
2020-09-11 14:38:00 +0200

    Add edit villager and modif profil

--- app/controllers/villagers_controller.rb
+++ app/controllers/villagers_controller.rb
@@ -1,4 +1,10 @@
 class VillagersController < ApplicationController
     def show
     end
+
+    def edit
+    end
+
+    def update
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', passwords: 'users/passwords', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :villagers, only: [:show]
+  resources :villagers, only: [:show, :edit, :update]
 
   resources :villages
   resources :events do
