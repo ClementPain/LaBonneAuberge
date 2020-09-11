8acf1eee126c80fdb7134d84c8b751564c6d5b3f
clement <clementpain@outlook.com>
2020-09-11 15:22:36 +0200

    small changes

--- app/controllers/manage_registration_town_halls_controller.rb
+++ app/controllers/manage_registration_town_halls_controller.rb
@@ -4,8 +4,12 @@ class ManageRegistrationTownHallsController < ApplicationController
 
   def create
     if @village = Village.find_by(zipcode:params[:zipcode])
-      @email = @village.email
-      redirect_to new_town_hall_registration_path(email: @email), notice: "Veuillez renseigner un mot de passe"
+      if !TownHall.find_by(email:@village.email)
+        @email = @village.email
+        redirect_to new_town_hall_registration_path(email: @email), notice: "Veuillez renseigner un mot de passe"
+      else
+        redirect_to new_manage_registration_town_hall_path, alert: "Cette ville a déjà un compte mairie"
+      end
     else
       redirect_to new_manage_registration_town_hall_path, alert: "Le zip code renseigné n'existe pas"
     end
