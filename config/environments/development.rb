770cdb7a4045d0da1280bbaba9400918ff159d71
oumasma <oumasma93@hotmail.fr>
2020-09-11 15:36:18 +0400

    config mailjet for test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
  
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -60,9 +58,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-
+  config.action_mailer.perform_caching = false
 end
