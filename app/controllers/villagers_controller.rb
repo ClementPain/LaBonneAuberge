d4d5f7b25d4b9e1925b61ad6ff2d6b0bca0d1389
Camillia-MEDDAHI <meddah.cam@laposte.net>
2020-09-14 11:42:40 +0200

    Profil villager edit

--- app/controllers/villagers_controller.rb
+++ app/controllers/villagers_controller.rb
@@ -2,14 +2,16 @@ class VillagersController < ApplicationController
 before_action :find_villager, only: [:edit, :update, :destroy, :show]
 
     def show
+        @villager = Villager.find(params[:id])
     end
 
     def edit
+        @villager = Villager.find(params[:id])
     end
 
     def update
         if @villager.update(villager_params)
-            redirect_to villager_path(@villager), notice: "Les informations de #{@villager.title} ont bien été mises à jour"
+            redirect_to villager_path(@villager), notice: "Les informations de #{@villager.first_name} ont bien été mises à jour"
         else
             redirect_to edit_villager_path(@villager), alert: "Veuillez renseigner toutes les informations"
         end
