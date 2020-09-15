156ab5e86eec472df495243af32a13fca9c0e7ea
oumasma <oumasma93@hotmail.fr>
2020-09-15 12:39:01 +0400

    add comment view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,28 @@
 class CommentsController < ApplicationController
   def index
+    @comments = Comment.all
   end
 
   def new
+    @comment = Comment.new
   end
 
   def create
+
+    @comment = Comment.new('villager_id' => Villager.find_by(first_name: params[:villager_first_name]).id,
+                      'event_id' => params[:event_id],
+                     'content' => params[:comment_content])
+    
+      if @comment.save
+        redirect_to new_event_comment_path(@comment.event_id), notice: "Le commentaire a bien été créé"
+        
+        
+      else
+        redirect_to new_event_comment_path, alert: "Certaines informations sont incorrectes"
+      end
+
+    
+      
   end
 
   def show
@@ -19,4 +36,14 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+  private
+  
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
+    def find_comment
+        @comment = Comment.find(params[:id])
+    end
 end
