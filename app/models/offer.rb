97ac56b2f009952eb38825aacef28cabc6eaaa62
clement <clementpain@outlook.com>
2020-09-16 17:59:32 +0200

    sidebar

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
     has_one_attached :offer_picture
     belongs_to :village
-    belongs_to :category
+    belongs_to :category, optional: true
 
 	validates :title, presence: true, length: { in: 5..140, message: "Un titre fait entre 5 et 140 caractères" }
 	validates :description, presence: true, length: { in: 10..1000, message: "Un description fait entre 10 et 1000 caractères" }
