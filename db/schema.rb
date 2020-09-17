82449d99c2cb7ab4bce40efad06f8240082a3b6f
oumasma <oumasma93@hotmail.fr>
2020-09-17 23:46:58 +0400

    add event to seeds

--- db/schema.rb
+++ db/schema.rb
@@ -143,6 +143,15 @@ ActiveRecord::Schema.define(version: 2020_09_17_140558) do
     t.index ["village_id"], name: "index_offers_on_village_id"
   end
 
+  create_table "offers_categories", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "offer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_offers_categories_on_category_id"
+    t.index ["offer_id"], name: "index_offers_categories_on_offer_id"
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -194,7 +203,7 @@ ActiveRecord::Schema.define(version: 2020_09_17_140558) do
     t.string "first_name"
     t.string "last_name"
     t.text "description"
-    t.datetime "date_of_birth"
+    t.date "date_of_birth"
     t.bigint "village_id"
     t.string "address"
     t.bigint "user_id"
@@ -219,6 +228,8 @@ ActiveRecord::Schema.define(version: 2020_09_17_140558) do
   add_foreign_key "forums", "villages"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "villagers"
+  add_foreign_key "offers_categories", "categories"
+  add_foreign_key "offers_categories", "offers"
   add_foreign_key "validation_town_halls", "villagers"
   add_foreign_key "validation_town_halls", "villages"
   add_foreign_key "villagers", "users"
